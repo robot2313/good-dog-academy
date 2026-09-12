1568e26e36f51ced39568d7e5a7a6d3b23e6b632
robot2313 <rfromme987@gmail.com>
2026-09-13 00:44:14 +0930

    Test next-rep voice intent

--- src/domain/behaviour/OwnerVoiceIntent.test.ts
+++ src/domain/behaviour/OwnerVoiceIntent.test.ts
@@ -6,6 +6,8 @@ describe('parseOwnerVoiceIntent', () => {
     ['yep, he did it', 'success'],
     ['that was almost right', 'partial-success'],
     ["no, he didn't do it", 'unsuccessful'],
+    ['ready for the next rep', 'next-rep'],
+    ['next', 'next-rep'],
     ['repeat that please', 'repeat'],
     ['stop the session', 'stop'],
   ])('maps %s to %s', (transcript, expected) => {
@@ -14,6 +16,11 @@ describe('parseOwnerVoiceIntent', () => {
 
   it('prioritises a stop request over scoring language', () => {
     expect(parseOwnerVoiceIntent('yes good, but stop the session')).toBe('stop');
+  });
+
+  it('keeps repeat distinct from moving to the next rep', () => {
+    expect(parseOwnerVoiceIntent('say that again')).toBe('repeat');
+    expect(parseOwnerVoiceIntent('ready for next')).toBe('next-rep');
   });
 
   it('fails closed when language is ambiguous', () => {
